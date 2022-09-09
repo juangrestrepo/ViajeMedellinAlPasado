7bd417c85a23e74db4fd4acbb5e068a154a0eb4f
Juan <juan.garcia142@udea.edu.co>
2022-09-08 21:43:24 -0500

    Puntaje por encontrar objetos

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
 
+    int cantidad = 0;
+    bool num1 = false;
+    bool num2 = false;
+
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
@@ -14,5 +18,21 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+        if(controller.transform.position.x > 15 && controller.transform.position.x < 25 && controller.transform.position.z > -38 && controller.transform.position.z < -30){
+            if(num1 == false){
+                cantidad++;
+                num1 = true;
+            }
+            Debug.Log("Estás dentro");
+        }
+        if(controller.transform.position.x > 0 && controller.transform.position.x < 5 && controller.transform.position.z > 0 && controller.transform.position.z < 5){
+            if(num2 == false){
+                cantidad++;
+                num2 = true;
+            }
+            Debug.Log("Estás dentro");
+        }
+        Debug.Log("CANTIDAD: "+cantidad+"  "+controller.transform.position.x + "  " + controller.transform.position.z);
     }
+
 }
